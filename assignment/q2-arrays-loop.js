98f10f5fb2210e4fc32c7e13373c195444fe9925
rkapril <rkapril@me.com>
2023-02-28 22:38:02 +0800

    align assignment q2.js

--- assignment/q2-arrays-loop.js
+++ assignment/q2-arrays-loop.js
@@ -8,7 +8,7 @@ const studentList = ["Johnson", "Christina", "Michael", "Lina", "Larry", "Michel
 
 function printStudentNames(){
     // Add code here
-    for(let i=0;i<studentList.length;i++){
+    for(let i=0; i<studentList.length; i++){
         console.log(studentList[i])
     }
 }
